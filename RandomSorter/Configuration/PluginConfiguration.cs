2e09bcf9b263718d0add7dfa307009efa8d85520
edupuis <etienne.dupuis@ec-lyon.fr>
2021-04-23 16:59:39 +0200

    refactor name from example

--- RandomSorter/Configuration/PluginConfiguration.cs
+++ RandomSorter/Configuration/PluginConfiguration.cs
@@ -1,6 +1,6 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.Template.Configuration
+namespace Jellyfin.Plugin.RandomSorter.Configuration
 {
     public enum SomeOptions
     {
--- RandomSorter/Plugin.cs
+++ RandomSorter/Plugin.cs
@@ -1,22 +1,26 @@
 using System;
 using System.Collections.Generic;
-using Jellyfin.Plugin.Template.Configuration;
+using Jellyfin.Plugin.RandomSorter.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template
+namespace Jellyfin.Plugin.RandomSorter
 {
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        public override string Name => "Template";
+        public override string Name => "Random Sorter";
 
-        public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+        private readonly Guid _id = new Guid("9eda27ec-6e19-41ff-89e6-cabeebaf3b14");
+        public override Guid Id => _id;
+
+        public override string Description => "Add random sorting for lazy night";
 
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
+            //public class NameComparer : IBaseItemComparer, IComparer<BaseItem>
         }
 
         public static Plugin Instance { get; private set; }
