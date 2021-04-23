46f8830644edec6656218479fe2c9936a30bfa68
edupuis <etienne.dupuis@ec-lyon.fr>
2021-04-23 17:54:15 +0200

    add randomsorter class

---
+++ RandomSorter/RandomSorter.cs
@@ -0,0 +1,30 @@
+using System;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Sorting;
+using MediaBrowser.Model.Querying;
+
+namespace Jellyfin.Plugin.RandomSorter
+{
+    /// <summary>
+    /// Class RandomComparer.
+    /// </summary>
+    public class RandomComparer : IBaseItemComparer
+    {
+        /// <summary>
+        /// Compares the specified x.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>System.Int32.</returns>
+        public int Compare(BaseItem x, BaseItem y)
+        {
+            return Guid.NewGuid().CompareTo(Guid.NewGuid());
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name => ItemSortBy.Random;
+    }
+}
